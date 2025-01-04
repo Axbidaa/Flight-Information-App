977cd5be69a4ea94770bb45cfe5ab45e3918d445
Axbidaa <nezumrashid@gmail.com>
2025-01-04 18:31:17 +0400

    feat: add the js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".submit").click(function () {
-        const api_key = '67793da13a45b414acd274ea'
+        const api_key = '67793da13a45b414acd274ea' // replace this with your api key. The current api key has 15 more requests that can be made.
         // input variables
         var flightString = $(".flight-number").val();
         var flightDate = $(".flight-date").val();
