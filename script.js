1e67692d078ddb4e708957762a11c1caf6438681
Axbidaa <nezumrashid@gmail.com>
2025-01-04 11:25:34 +0400

    feat: added input fields and completed the frontend part

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".submit").click(function () {
+        console.log("done");
+        $(".search-output").html('<p>New content added to the div!</p>');
+    });
+});
