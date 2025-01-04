c0c2af36b1ec592bb3711ec5effe5cc727b27d1b
Axbidaa <nezumrashid@gmail.com>
2025-01-04 11:25:42 +0400

    chore: added javascript file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $(".submit").click(function () {
-        console.log("done");
         $(".search-output").html('<p>New content added to the div!</p>');
     });
 });
