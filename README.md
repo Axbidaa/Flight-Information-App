e93a93841651a20eb01667cb7d52ca4b70234b63
Axbidaa <nezumrashid@gmail.com>
2025-01-04 18:31:12 +0400

    feat: created the backend to handle user input, make a request to the api and display the information

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Flight-Information-App
+
 A basic web application that retrieves flight information using an API
+
+## Can you not see the
--- script.js
+++ script.js
@@ -1,5 +1,78 @@
 $(document).ready(function () {
     $(".submit").click(function () {
-        $(".search-output").html('<p>New content added to the div!</p>');
+        const api_key = '67793da13a45b414acd274ea'
+        // input variables
+        var flightString = $(".flight-number").val();
+        var flightDate = $(".flight-date").val();
+        var regex = /^([A-Za-z]{2,3})(\d+)$/;
+        var match = flightString.match(regex);
+        var dateObj = new Date(flightDate);
+
+        // variables for the API
+        if (match) {
+            var flightCode = match[1];
+            var flightNumber = match[2];
+        }
+        else {
+            $(".search-output").html('<div class="error-message"><p>No Data Found. Please make sure the data entered is valid.</p></div>');
+            return;
+        }
+
+        var formattedDate = dateObj.getFullYear() +
+            String(dateObj.getMonth() + 1).padStart(2, '0') +
+            String(dateObj.getDate()).padStart(2, '0');
+
+        //send a request to the api
+        $.ajax({
+            url: `https://api.flightapi.io/airline/${api_key}`,
+            type: "GET",
+            data: {
+                num: flightNumber,
+                name: flightCode,
+                date: formattedDate
+            },
+            success: function (response) {
+                // display the information obtained from the api
+                var departure = response[0].departure[0];
+                var arrival = response[1].arrival[0];
+
+
+                var output = `
+                    <div class="search-output-header">
+                        <b>${flightString}</b> 
+                        <p>Flight Status: <b>${departure.status}</b></p> 
+                    </div>
+
+                    <div class="search-output-body">        
+                        <div class="search-output-departure">
+                            <b>Departure</b>
+                            ${generateDetails(departure)}
+                        </div>
+                        <div class="search-output-arrival">
+                            <b>Arrival</b>
+                            ${generateDetails(arrival)}
+                        </div>
+                    </div>`;
+
+                // Function to dynamically generate the key-value pairs
+                function generateDetails(data) {
+                    var detailsHtml = '';
+                    $.each(data, function (key, value) {
+                        if (key != 'status') {
+                            detailsHtml += `<p><strong>${key}</strong> ${value}</p>`;
+                        }
+                    });
+                    return detailsHtml;
+                }
+
+                $(".search-output").html(output);
+            },
+            error: function (error) {
+                $(".search-output").html('<div class="error-message"><p>No Data Found. Please make sure the data entered is valid.</p></div>');
+                return;
+            }
+        });
+
+
     });
 });
